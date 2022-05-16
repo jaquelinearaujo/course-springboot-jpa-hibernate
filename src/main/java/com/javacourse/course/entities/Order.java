fb38d8b91aad81839bd9f3c64f7a1c920b0e57d6
jaquelinearaujo <jaquelineluiza4444@gmail.com>
2022-05-16 09:16:26 -0300

    main: Add OrderStatus enum

--- src/main/java/com/javacourse/course/entities/Order.java
+++ src/main/java/com/javacourse/course/entities/Order.java
@@ -1,12 +1,14 @@
 package com.javacourse.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javacourse.course.entities.enuns.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Entity
@@ -21,7 +23,24 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (Objects.nonNull(orderStatus)) this.orderStatus = orderStatus.getCode();
+    }
 }
