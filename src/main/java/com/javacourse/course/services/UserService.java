0e9c2d8f3560d697c742fc7e47e31d98fe8d2834
jaquelinearaujo <jaquelineluiza4444@gmail.com>
2022-05-21 13:58:33 -0300

    main: Add exception handling - update

--- src/main/java/com/javacourse/course/services/UserService.java
+++ src/main/java/com/javacourse/course/services/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
@@ -42,14 +43,18 @@ public class UserService implements Serializable {
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
         } catch (DataIntegrityViolationException e) {
-           throw new DatabaseException(e.getMessage());
+            throw new DatabaseException(e.getMessage());
         }
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
